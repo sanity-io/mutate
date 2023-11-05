a6800f2a5ab6876448aabda8743be8c85bd9db9f
Bjørge Næss <bjoerge@gmail.com>
2023-11-06 00:43:51 +0100

    fix: fix broken export

--- src/_unstable_apply.ts
+++ src/_unstable_apply.ts
@@ -1,7 +1,1 @@
-export {stringify} from './path/parser/stringify'
-export {parse} from './path/parser/parse'
-export type * from './path/types'
-export type * from './path/parser/types'
-
-export * from './path/utils/normalize'
-export * from './path/utils/predicates'
+export * from './apply'
