37b951aa5cc140de477a8a6f26818fc32725a1f3
Bjørge Næss <bjoerge@gmail.com>
2023-11-25 19:43:31 +0100

    chore: remove query for now

--- src/store/contentLakeStore.ts
+++ src/store/contentLakeStore.ts
@@ -18,7 +18,6 @@ import {squashTransactions} from './optimizations/squashMutations'
 import {applyMendozaPatch} from './applyMendoza'
 import {rebase} from './rebase'
 import {squashDMPStrings} from './optimizations/squashDMPStrings'
-import {createLoader, query} from './query'
 import type {SanityDocumentBase} from '../mutations/types'
 import type {Observable} from 'rxjs'
 
@@ -108,16 +107,6 @@ export function createContentLakeStore(
     localLog: localLog$.asObservable(),
     remoteLog: remoteLog$.asObservable(),
     outbox: outbox$.asObservable().pipe(map(() => outbox)),
-    query: async (q: string, params?: Record<string, unknown>) => {
-      const start = new Date()
-      const dataset = local.getAll()
-      const loader = createLoader(backend.fetchDocuments, dataset)
-      const result = await query(dataset, loader, q, params)
-      return {
-        result,
-        ms: new Date().getTime() - start.getTime(),
-      }
-    },
     mutate: mutations => {
       outbox.push({mutations})
       const res = local.apply(mutations)
