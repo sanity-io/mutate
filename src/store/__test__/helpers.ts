63cefefabcfd990ca24a920902758e7091d8307f
Bjørge Næss <bjoerge@gmail.com>
2023-11-26 22:00:00 +0100

    {wip} saved 2023-11-26 22:00

--- src/store/__test__/helpers.ts
+++ src/store/__test__/helpers.ts
@@ -7,18 +7,18 @@ import {
   toArray,
 } from 'rxjs'
 
-export type NextEmission<T> = {type: 'next'; value: T}
-export type ErrorEmission = {type: 'error'; error: unknown}
-export type CompleteEmission = {type: 'complete'}
-export type Emission<T> = NextEmission<T> | ErrorEmission | CompleteEmission
+export type NextEmission<T> = {kind: 'NEXT'; value: T}
+export type ErrorEmission = {kind: 'ERROR'; error: unknown}
+export type CompleteEmission = {kind: 'COMPLETE'}
+export type Notification<T> = NextEmission<T> | ErrorEmission | CompleteEmission
 export function collectNotifications<T>(observable: Observable<T>) {
-  const emissions: Emission<T>[] = []
+  const emissions: Notification<T>[] = []
   const subscription = observable
     .pipe(
       tap({
-        next: value => emissions.push({type: 'next', value}),
-        error: error => emissions.push({type: 'error', error}),
-        complete: () => emissions.push({type: 'complete'}),
+        next: value => emissions.push({kind: 'NEXT', value}),
+        error: error => emissions.push({kind: 'ERROR', error}),
+        complete: () => emissions.push({kind: 'COMPLETE'}),
       }),
       catchError(error => {
         // console.log(new Date(), 'caught error', error)
