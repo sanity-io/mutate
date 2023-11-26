d2245ee8615d24ccbafe22395033a6cd21965172
Bjørge Næss <bjoerge@gmail.com>
2023-11-26 15:16:13 +0100

    feat(store): export unstable_store

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,1 +1,18 @@
 export * from './contentLakeStore'
+export type * from './types'
+
+/** Required support types */
+export type * from '../mutations/operations/types'
+export type {NodePatch, PatchOptions} from '../mutations/types'
+export type {
+  CreateIfNotExistsMutation,
+  CreateMutation,
+  CreateOrReplaceMutation,
+  DeleteMutation,
+  Mutation,
+  NodePatchList,
+  PatchMutation,
+  SanityDocumentBase,
+} from '../mutations/types'
+export type * from '../path'
+export type {Optional} from '../utils/typeUtils'
