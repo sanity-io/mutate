f6b75c5cd7abd691e2a0ee353dba0e69693d4ea1
Bjørge Næss <bjoerge@gmail.com>
2023-12-02 21:20:35 +0000

    fix: inline mutation event

---
+++ src/store/sanityApiTypes.ts
@@ -0,0 +1,19 @@
+import {type SanityMutation} from '../encoders/sanity'
+import {type SanityDocumentBase} from '../mutations/types'
+
+export type SanityMutationEvent = {
+  type: 'mutation'
+  documentId: string
+  eventId: string
+  identity: string
+  mutations: SanityMutation[]
+  previousRev?: string
+  resultRev?: string
+  result?: SanityDocumentBase
+  previous?: SanityDocumentBase | null
+  effects?: {apply: unknown[]; revert: unknown[]}
+  timestamp: string
+  transactionId: string
+  transition: 'update' | 'appear' | 'disappear'
+  visibility: 'query' | 'transaction'
+}
