7dd8b2de56308e18b8d1adaebfeeb073ed265461
Bjørge Næss <bjoerge@gmail.com>
2025-12-14 14:05:46 +0100

    refactor(examples): update web example demo

---
+++ examples/web/lib/textTyper.ts
@@ -0,0 +1,37 @@
+const typeCharacter = (
+  input: HTMLInputElement | HTMLTextAreaElement,
+  char: string,
+  position: number,
+) => {
+  const ev = new InputEvent('change', {
+    bubbles: true,
+    cancelable: false,
+  })
+  input.setRangeText(char, position ?? 1, position ?? 0, 'end')
+  input.dispatchEvent(ev)
+}
+
+export function startTyping(
+  input: HTMLInputElement | HTMLTextAreaElement,
+  text: string,
+  delay: (char: string) => number,
+  onEnd: () => void,
+) {
+  let charIdx = 0
+
+  let timerId = setTimeout(write, 0)
+
+  function write() {
+    const char = text[charIdx]
+    typeCharacter(input, char || '', input.selectionStart ?? 0)
+    charIdx++
+    if (charIdx < text.length) {
+      timerId = setTimeout(write, delay(char!))
+    } else {
+      onEnd()
+    }
+  }
+  return () => {
+    clearTimeout(timerId)
+  }
+}
