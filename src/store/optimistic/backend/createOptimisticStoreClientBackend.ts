1bd227f4a646a47a240a0dafe9ebf59af7ee0156
Bjørge Næss <bjoerge@gmail.com>
2025-12-15 00:33:07 +0100

    wip: use mimimal interface for sanity client

--- src/store/optimistic/backend/createOptimisticStoreClientBackend.ts
+++ src/store/optimistic/backend/createOptimisticStoreClientBackend.ts
@@ -1,15 +1,35 @@
-import {type SanityClient} from '@sanity/client'
-import {from} from 'rxjs'
+import {
+  type BaseMutationOptions,
+  type QueryParams,
+  type RequestOptions,
+} from '@sanity/client'
+import {from, type Observable} from 'rxjs'
 
 import {encodeTransaction} from '../../../encoders/sanity'
 import {type Transaction} from '../../../mutations/types'
 import {createDocumentEventListener} from '../../listeners/createDocumentEventListener'
 import {createDocumentLoaderFromClient} from '../../listeners/createDocumentLoader'
 import {createSharedListenerFromClient} from '../../listeners/createSharedListener'
+import {type ListenerEndpointEvent, type SubmitResult} from '../../types'
 import {type OptimisticStoreBackend} from '../createOptimisticStore'
 
+export interface SanityClientLike {
+  dataRequest(
+    endpoint: string,
+    body: unknown,
+    options?: BaseMutationOptions,
+  ): Promise<SubmitResult>
+  getDataUrl(doc: string, s: string): string
+  observable: {request<T>(options: RequestOptions): Observable<T>}
+  listen(
+    query: string,
+    queryParams: QueryParams,
+    request: RequestOptions,
+  ): Observable<ListenerEndpointEvent>
+}
+
 export function createOptimisticStoreClientBackend(
-  client: SanityClient,
+  client: SanityClientLike,
 ): OptimisticStoreBackend {
   const listenDocument = createDocumentEventListener({
     loadDocument: createDocumentLoaderFromClient(client),
